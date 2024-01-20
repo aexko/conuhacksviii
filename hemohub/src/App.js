117afff7c0104c741de146c53d827665b9938b6c
Lenovo-PC-2 <43526682+ltJustWorks@users.noreply.github.com>
2024-01-20 18:41:37 -0500

    add react router, css

--- hemohub/src/App.js
+++ hemohub/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import HemoSurvey from './components/HemoSurvey';
 import HomePage from './components/HomePage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<HomePage />} />
+          <Route path="survey" element={<HemoSurvey />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- hemohub/src/components/HomePage.js
+++ hemohub/src/components/HomePage.js
@@ -1,9 +1,12 @@
 import './css/HomePage.css'
+import { useNavigate } from 'react-router-dom'
 
 const HomePage = () => {
+    const navigate = useNavigate()
+
     return (
-        <div>
-            <button>Check eligibility</button>
+        <div className="home-container">
+            <button className="home-button" onClick={() => navigate('/survey')}>Check eligibility</button>
 
             <div className='stats-section'>stats</div>
         </div>
