b71b968de51326925c80de566d432e624f1a0f1a
Lenovo-PC-2 <43526682+ltJustWorks@users.noreply.github.com>
2024-01-20 20:28:47 -0500

    add survey done component

--- hemohub/src/App.js
+++ hemohub/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HemoSurvey from './components/HemoSurvey';
 import HomePage from './components/HomePage';
+import SurveyDone from './components/SurveyDone';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Route path="/">
           <Route index element={<HomePage />} />
           <Route path="survey" element={<HemoSurvey />} />
+          <Route path="survey-done" element={<SurveyDone />} />
         </Route>
       </Routes>
     </BrowserRouter>
